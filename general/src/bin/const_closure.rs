01b83b7ff63b1342e18bd08fbfc1040bd5228d3c
pvillela <pvillela@gmail.com>
2023-01-28 23:28:16 -0500

    Improved const_closure example.

--- general/src/bin/const_closure.rs
+++ general/src/bin/const_closure.rs
@@ -3,8 +3,19 @@ fn const_closure<T: 'static + Clone>(x: T) -> impl Fn() -> T {
     move || x.clone()
 }
 
+/// Transforms a value into a nullary closure that returns the value but can only be called once.
+fn const_once_closure<T: 'static>(x: T) -> impl FnOnce() -> T {
+    move || x
+}
+
 fn main() {
     let f = const_closure(1);
-    let x = f();
-    println!("x: {}", x);
+    let x1 = f();
+    let x2 = f();
+    println!("x1: {}, x2: {}", x1, x2);
+
+    let f_o = const_once_closure(2);
+    let y1 = f_o();
+    // let y2 = f_o(); // doesn't compile because f_o is FnOnce.
+    println!("y1: {}", y1);
 }

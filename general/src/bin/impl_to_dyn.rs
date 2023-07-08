86abeb91e0d873d1a2ac5a3c79d8b6f58ff7705c
pvillela <pvillela@gmail.com>
2023-07-08 11:21:20 -0400

    Improved impl_to_dyn example.

--- general/src/bin/impl_to_dyn.rs
+++ general/src/bin/impl_to_dyn.rs
@@ -1,4 +1,4 @@
-//! This file demonstrates conversion of impl to dyn.
+//! This file demonstrates conversion of impl to dyn and &dyn.
 
 #![allow(unused)]
 
@@ -16,37 +16,33 @@ impl Foo for Bar {
 }
 
 fn main() {
-    let s = "42".to_owned();
-    let s_lr: &String = &Box::new(s);
-
-    let str = "84".to_owned();
-    let f = move || str.clone();
-    let f_ib: Box<dyn Fn() -> String> = Box::new(f); // conversion from Box<impl> to Box<dyn>
-    let f_lr: &dyn Fn() -> String = &f_ib;
-
-    let b = Bar(42);
-    let b_ib: Box<dyn Foo> = Box::new(b); // conversion from Box<impl> to Box<dyn>
-    let b_lr: &dyn Foo = b_ib.as_ref(); // &b_ib doesn't compile
-
-    // Attempt to convert Box<&dyn> to Box<dyn> doesn't compile
-    // let b_lrb: Box<dyn Foo> = Box::new(b_lr); // doesn't compile
-
-    println!("{}", s_lr);
-    println!("{}", f_lr());
-
     {
-        let s_lrb: Box<&String> = Box::new(s_lr);
-        println!("{}", s_lrb);
+        let str = "42".to_owned();
+        let f = move || str.clone();
+        let f_box_impl = Box::new(f);
+        let f_box_dyn: Box<dyn Fn() -> String> = f_box_impl;
+        let f_ref_dyn: &dyn Fn() -> String = f_box_dyn.as_ref();
+        let f_ref_dyn: &dyn Fn() -> String = &*f_box_dyn;
+        let f_ref_dyn: &dyn Fn() -> String = &f_box_dyn; // works for closure only
+        let f_ref_dyn_leaked: &dyn Fn() -> String = Box::leak(f_box_dyn);
 
-        let f_lrb: Box<&dyn Fn() -> String> = Box::new(f_lr);
-        // Conversion from Box<&dyn> to Box<dyn> works for closures but not other traits
-        let f_lrb1: Box<dyn Fn() -> String> = Box::new(f_lr);
-        let f_lrb2: Box<dyn Fn() -> String> = f_lrb;
-        // Conversion in the other direction doesn't work
-        // let f_lrb3: Box<&dyn Fn() -> String> = f_lrb2; //doesn't compile
-        println!("{}", f_lrb1());
+        let str = "42".to_owned();
+        let f = move || str.clone();
+        let f_box_impl = Box::new(f);
+        let f_ref_dyn_leaked: &dyn Fn() -> String = Box::leak(f_box_impl);
     }
 
-    println!("{}", s_lr);
-    println!("{}", f_lr());
+    {
+        let b = Bar(84);
+        let b_box_impl = Box::new(b);
+        let b_box_dyn: Box<dyn Foo> = b_box_impl;
+        let b_ref_dyn: &dyn Foo = b_box_dyn.as_ref();
+        let b_ref_dyn: &dyn Foo = &*b_box_dyn;
+        // let b_reb_dyn: &dyn Foo = &b_box_dyn; // works for closure only
+        let b_ref_dyn_leaked: &dyn Foo = Box::leak(b_box_dyn);
+
+        let b = Bar(84);
+        let b_box_impl = Box::new(b);
+        let b_ref_dyn_leaked: &dyn Foo = Box::leak(b_box_impl);
+    }
 }

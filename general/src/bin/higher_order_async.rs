e74519f322d113b6677ff309ad5597343dfea32f
pvillela <pvillela@gmail.com>
2023-07-09 23:49:13 -0400

    Added polymorphic example to higher_order_async.

--- general/src/bin/higher_order_async.rs
+++ general/src/bin/higher_order_async.rs
@@ -1,14 +1,26 @@
-//! This example demonstrates the creation of a higher order function accepting an async function that
-//! itself takes a reference argument.
-//! See https://github.com/rust-lang/rust/issues/113495
-//! and https://github.com/rust-lang/rust/issues/113495#issuecomment-1627893701.
+//! This file demonstrates two things:
+//!
+//! 1. the creation of a higher order function accepting an async function that
+//!    itself takes a reference argument.
+//!
+//!    See https://github.com/rust-lang/rust/issues/113495
+//!    and https://github.com/rust-lang/rust/issues/113495#issuecomment-1627893701.
+//!
+//! 2. How a polymorphic function can be passed to a higher-order function that accepts
+//!    a function whose arguments' types are compatible with those of the polymorphic
+//!    function.
 
 use core::future::Future;
+use std::fmt::Debug;
 
 #[tokio::main]
 async fn main() {
-    _ = higher_order_tx(f_tx).await;
+    higher_order_tx(f_tx).await;
+    higher_order(f_poly, 84).await;
 }
+
+//==========
+// Example 1
 
 trait Tx {
     fn show(&self);
@@ -16,7 +28,7 @@ trait Tx {
 
 impl Tx for u32 {
     fn show(&self) {
-        println!("Value = {self}");
+        println!("Tx for u32 value = {self}");
     }
 }
 
@@ -32,4 +44,19 @@ where
     Fut: Future<Output = ()>,
 {
     f(&42u32).await;
+}
+
+//==========
+// Example 2
+
+async fn higher_order<Fut>(f: impl Fn(u32) -> Fut, input: u32)
+where
+    Fut: Future<Output = ()>,
+{
+    f(input).await;
+}
+
+async fn f_poly(x: impl Debug) {
+    println!("f_poly executed");
+    println!("Poly value = {:?}", x);
 }

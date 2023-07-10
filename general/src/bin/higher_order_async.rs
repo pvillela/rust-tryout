e4651493171c6cf9613c8d579e34a9ed76394cdd
pvillela <pvillela@gmail.com>
2023-07-09 21:03:14 -0400

    Simplified higher_order_async example.

--- general/src/bin/higher_order_async.rs
+++ general/src/bin/higher_order_async.rs
@@ -1,71 +1,35 @@
-//! Demonstrates higher-order functions that takes an async fn as an argument.
+//! This example demonstrates the creation of a higher order function accepting an async function that
+//! itself takes a reference argument.
 //! See https://github.com/rust-lang/rust/issues/113495
-//! and https://github.com/rust-lang/rust/issues/113495#issuecomment-1627640952.
+//! and https://github.com/rust-lang/rust/issues/113495#issuecomment-1627893701.
 
-use std::future::Future;
+use core::future::Future;
 
-struct X;
-
-trait Tx {}
-
-fn main() {
-    higher_order(f_x);
-    higher_order_x(f_x);
-
-    higher_order(f_tx);
-    // higher_order_tx(f_tx); // does not compile
-
-    higher_order_tx_nf(f_tx_nf);
-
-    higher_order_tx_corrected(f_tx);
+#[tokio::main]
+async fn main() {
+    _ = higher_order_tx(f_tx).await;
 }
 
-fn higher_order<I, FUT>(_f: fn(I) -> FUT)
+trait Tx {
+    fn show(&self);
+}
+
+impl Tx for u32 {
+    fn show(&self) {
+        println!("Value = {self}");
+    }
+}
+
+async fn f_tx(input: &dyn Tx) {
+    println!("f_tx executed");
+    input.show();
+}
+
+/// The lifetime parameter `'a` is criticallu important. Without it, the above call in main generates
+/// a compilation error.
+async fn higher_order_tx<'a, Fut>(f: fn(&'a dyn Tx) -> Fut)
 where
-    FUT: Future<Output = ()>,
+    Fut: Future<Output = ()>,
 {
+    f(&42u32).await;
 }
-
-fn higher_order_x<FUT>(_f: fn(X) -> FUT)
-where
-    FUT: Future<Output = ()>,
-{
-}
-
-/// Doesn't work, see [higher_order_tx_corrected] below.
-#[allow(unused)]
-fn higher_order_tx<FUT>(_f: fn(&dyn Tx) -> FUT)
-where
-    FUT: Future<Output = ()>,
-{
-}
-
-fn higher_order_tx_nf(_f: fn(&dyn Tx) -> ()) {}
-
-async fn f_x(_input: X) {}
-
-async fn f_tx(_input: &dyn Tx) {}
-
-fn f_tx_nf(_input: &dyn Tx) {}
-
-////
-// Fix to type issue, presented in https://github.com/rust-lang/rust/issues/113495#issuecomment-1627640952
-
-trait AsyncBorrowFn<'a, A: ?Sized + 'a>: Fn(&'a A) -> Self::Fut {
-    type Out;
-    type Fut: Future<Output = Self::Out> + 'a;
-}
-
-impl<'a, A, F, Fut> AsyncBorrowFn<'a, A> for F
-where
-    A: ?Sized + 'a,
-    F: Fn(&'a A) -> Fut,
-    Fut: Future + 'a,
-{
-    type Out = Fut::Output;
-    type Fut = Fut;
-}
-
-impl Tx for u32 {}
-
-fn higher_order_tx_corrected(_f: impl for<'a> AsyncBorrowFn<'a, dyn Tx + 'a, Out = ()>) {}

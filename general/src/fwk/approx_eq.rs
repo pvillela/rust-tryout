427da85b0df78ca6268a611b79d43a6ddbdf30c3
pvillela <pvillela@gmail.com>
2025-03-06 08:51:44 -0500

    Added general::fwk::approx_eq trait for floating point integers.

---
+++ general/src/fwk/approx_eq.rs
@@ -0,0 +1,51 @@
+//! Provides approximate equality for floating point types
+
+pub trait ApproxEq {
+    fn approx_eq(self, other: Self, epsilon: Self) -> bool;
+}
+
+impl ApproxEq for f32 {
+    fn approx_eq(self, other: Self, epsilon: Self) -> bool {
+        (self - other).abs() <= epsilon
+    }
+}
+
+impl ApproxEq for f64 {
+    fn approx_eq(self, other: Self, epsilon: Self) -> bool {
+        (self - other).abs() <= epsilon
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::ApproxEq;
+
+    #[test]
+    fn test() {
+        {
+            let x32: f32 = 123.444444;
+            let y32: f32 = 123.444454;
+            let z32: f32 = 123.444455;
+            let epsilon: f32 = 0.00001;
+
+            assert!(x32.approx_eq(y32, epsilon), "x32 must be approx_eq to y32");
+            assert!(
+                !x32.approx_eq(z32, epsilon),
+                "x32 must not be approx_eq to z32"
+            );
+        }
+
+        {
+            let x64: f64 = 123.444444;
+            let y64: f64 = 123.444454;
+            let z64: f64 = 123.444455;
+            let epsilon: f64 = 0.00001;
+
+            assert!(x64.approx_eq(y64, epsilon), "x64 must be approx_eq to y64");
+            assert!(
+                !x64.approx_eq(z64, epsilon),
+                "x64 must not be approx_eq to z64"
+            );
+        }
+    }
+}
--- general/src/fwk/mod.rs
+++ general/src/fwk/mod.rs
@@ -1,6 +1,7 @@
 mod higher_order_functions;
 pub use higher_order_functions::*;
 
+pub mod approx_eq;
 pub mod btreemap_ext;
 pub mod map_ext_owned;
 pub mod map_ext_ref;

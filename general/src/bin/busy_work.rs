9296aaef55781183c93f659cf138fdeb00172104
pvillela <pvillela@gmail.com>
2025-07-03 02:01:26 -0400

    Changed busy_work::latency_m to use an array on the stack to store latencies.

--- general/src/bin/busy_work.rs
+++ general/src/bin/busy_work.rs
@@ -64,22 +64,29 @@ pub fn latency(f: impl FnOnce()) -> Duration {
     Instant::now().duration_since(start)
 }
 
-/// Invokes `f` `r*2 + 1` times and returns the median latency.
+/// Invokes `f` `R` times and returns the median latency.
 #[inline(always)]
-fn latency_m(f: impl Fn(), r: u8) -> Duration {
-    if r == 0 {
+fn latency_m<const R: usize>(f: impl Fn()) -> Duration {
+    if R <= 1 {
         return latency(&f);
     }
 
-    let size = (2 * r + 1) as usize;
-    let mut lats = Vec::<Duration>::with_capacity(size);
+    let mut lats = [Duration::new(0, 0); R];
 
-    for _ in 0..size {
-        lats.push(latency(&f));
+    for i in 0..R {
+        lats[i] = latency(&f);
     }
 
     comb_sort(&mut lats);
-    lats[r as usize]
+
+    if R % 2 == 1 {
+        lats[R / 2]
+    } else {
+        let m1 = lats[R / 2 - 1].as_nanos();
+        let m2 = lats[R / 2].as_nanos();
+        let m = (m1 + m2) / 2;
+        Duration::from_nanos(m as u64)
+    }
 }
 
 /// Function that does a significant amount of computation to support validation of benchmarking frameworks.
@@ -137,26 +144,28 @@ pub fn busy_work_exp(effort: u32) {
 
 /// Returns an estimate of the number of iterations required for `busy_work` to have latency `target_latency`.
 ///
-/// Calls [`calibrate_busy_work_x`] with a predefined default `calibration_effort` of `200_000` and `r` value of `0` (1 run);
+/// Calls [`calibrate_busy_work_x`] with predefined default `calibration_effort` and `R` values.
 pub fn calibrate_busy_work(busy_work: fn(u32), target_latency: Duration) -> u32 {
-    const CALIBRATION_EFFORT: u32 = 200_000;
-    const R: u8 = 0;
-    calibrate_busy_work_x(busy_work, target_latency, CALIBRATION_EFFORT, R)
+    const CALIBRATION_EFFORT: u32 = 100_000;
+    const R: usize = 0;
+    calibrate_busy_work_x::<R>(busy_work, target_latency, CALIBRATION_EFFORT)
 }
 
 /// Returns an estimate of the number of iterations required for `busy_work` to have latency `target_latency`.
+///
+/// # Generic parameters:
+/// - `R`: the number of times the calibration is run. The median calibration is returned. An extremely high value
+///   for `R` will cause a stack overflow.
 ///
 /// # Arguments
 /// - `busy_work`: function to be calibrated.
 /// - `target_latency`: target latency.
 /// - `calibration_effort`: the number of iterations executed during calibration.
-/// - `r`: the calibration is run `r*2 + 1` times and the median value is returned.
-pub fn calibrate_busy_work_x(
+pub fn calibrate_busy_work_x<const R: usize>(
     busy_work: fn(u32),
     target_latency: Duration,
     calibration_effort: u32,
-    r: u8,
 ) -> u32 {
-    let latency = latency_m(|| busy_work(calibration_effort), r);
+    let latency = latency_m::<R>(|| busy_work(calibration_effort));
     (target_latency.as_nanos() * calibration_effort as u128 / latency.as_nanos()) as u32
 }
